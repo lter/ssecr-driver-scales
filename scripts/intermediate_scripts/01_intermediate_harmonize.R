af63750c8502f5582a210bee5b9b0242f7bf0426
allie-case <caseallie16@gmail.com>
2025-04-16 15:04:27 -0400

    edit: made it through steps 1 and 2 of LTER sites for harmonization process. There are a total of 85 taxonomic differences to address, got it down to 49 at the end of today.

--- scripts/intermediate_scripts/01_intermediate_harmonize.R
+++ scripts/intermediate_scripts/01_intermediate_harmonize.R
@@ -43,9 +43,15 @@ unique(harmonized$SITE)
 
 #latest update to sites is that we are splitting up the MCR and SBC sites for LTER. 
 
-# harmonized %>% 
-#   (SUBSITE = case_when(SITE == "",
-#                        .default = SITE))
+harmonized <- harmonized %>%
+  mutate(MIDSITE = case_when(
+    str_detect(SUBSITE, "Backreef") ~ "LTER_MCR_Backreef",
+    str_detect(SUBSITE, "Fringing Reef") ~ "LTER_MCR_FringingReef",
+    str_detect(SUBSITE, "Forereef") ~ "LTER_MCR_Forereef",
+    TRUE ~ SITE)) %>% 
+  relocate(MIDSITE, .after = SITE)
+
+unique(harmonized$MIDSITE)
 
 #Bring in taxon list (first from NEON then can append LTER in later)
 
@@ -55,11 +61,7 @@ taxon <- read.csv(file = file.path("data",
 taxon <-  taxon %>% 
   select(taxonID, acceptedTaxonID, scientificName, vernacularName, taxonRank)
 
-#bring in the LTER taxon list 
-
-
-  
-## STEP 1: CHECK AGAINST OFFICIAL TAXONOMIC LIST FOR TYPOS/MISMATCHES ---- 
+## NEON STEP 1: CHECK AGAINST OFFICIAL TAXONOMIC LIST FOR TYPOS/MISMATCHES ---- 
 
 #filter only NEON 
 
@@ -76,24 +78,9 @@ setdiff(NEON_data$SCI_NAME,
 setdiff(NEON_data$SP_CODE,
         taxon$acceptedTaxonID) #all codes match 
 
-#filter only LTER
-
-LTER_data <- harmonized %>% 
-  filter(grepl('LTER', SITE))
-
-#check it picked up only NEON sites and first check for obvious mismatches or typos
-
-unique(LTER_data$SITE)
-
-setdiff(LTER_data$SCI_NAME,
-        taxon$scientificName) #all scientific names match
-
-setdiff(LTER_data$SP_CODE,
-        taxon$acceptedTaxonID) #all codes match 
-
 #now check our data for any fish not at a species or subspecies level - in the taxonomic guide this will be helpful to check the taxonRank.
 
-## FILTER FOR ONLY SUBSPECIES & SPECIES ---- 
+## NEON STEP 2: FILTER FOR ONLY SUBSPECIES & SPECIES ---- 
 
 #pull our data that does not have a species or subspecies taxon rank
 
@@ -135,12 +122,12 @@ final_drop_table <- total_rows_site %>%
 
 final_drop_table #looks like right now only one of concern is NEON_CRAM.
 
-## DROP DATA BASED ON TAXON FILTERING ------
+## NEON STEP 3: DROP DATA BASED ON TAXON FILTERING ------
 
 NEON_data <- NEON_data %>% 
   filter(!SCI_NAME %in% ranks$scientificName)
 
-## "RARE" SPECIES ------
+## NEON STEP 4: "RARE" SPECIES ------
 
 #at each site, figure out if there are any species that have occurred only 1 or 2 times. We will want to drop these. 
 
@@ -190,16 +177,18 @@ final_drop_table_rare <- total_rows_rare %>%
 final_drop_table_rare #looks like right now only one of concern is NEON_POSE.
 
 
-##DROP DATA BASED ON SPECIES FREQUENCY/RARE SPECIES ----
+## NEON STEP 5: DROP DATA BASED ON SPECIES FREQUENCY/RARE SPECIES ----
 
 NEON_data <- NEON_data %>% 
   mutate(combo = paste(SITE,"_",SCI_NAME)) %>% 
   filter(!combo %in% rare_drops$combo) %>% 
   select(!combo)
 
-##APRIL UPDATE: DROP FROM SP'S GOOGLE DRIVE NOTES ----
+##APRIL UPDATE: DROP FROM SP'S GOOGLE DRIVE NOTES 
 
 #These are sites that Sierra picked out to drop based on environmental variables not being available for > 5 years 
+
+## NEON STEP 6: DROP DATA WITH <5 YEARS OF ENVIRONMENTAL DATA & FINAL DROPS
 
 site_drops <- c("NEON_CARI",
                 "NEON_PRLA",
@@ -212,9 +201,19 @@ site_drops <- c("NEON_CARI",
 NEON_data <- NEON_data %>% 
   filter(!(SITE %in% site_drops))
 
-#FINAL HARMONIZATION ----
+#there are also a couple sites where we're only dropping some of the variables:
 
-NEON_harmonized <- NEON_data
+#drop DO from NEON CRAM AND NEON LECO
+NEON_data <- NEON_data %>%
+  mutate(across(contains("DO"),
+                ~ if_else(SITE %in% c("NEON_CRAM", "NEON_LECO"), NA_real_, .)))
+#
+# #drop temp from NEON KING
+#
+NEON_data <- NEON_data %>%
+  mutate(across(contains("temp"),
+                ~ if_else(SITE %in% c("NEON_KING"), NA_real_, .)))
+
 
 #save as Rds to use in model scripts and PDF viz
 
@@ -222,20 +221,155 @@ saveRDS(NEON_harmonized,
         file = file.path("data",
                          "NEON_harmonized.Rds"))
 
-#these all work, but now the issue is the PDF loops don't all work because it can't loop the same plots across each plot. Skip for now and work on later. 
+#FINAL JOINT HARMONIZATION ----
 
-#there are also a couple sites where we're only dropping some of the variables: 
+NEON_harmonized <- NEON_data
 
-#drop DO from NEON CRAM AND NEON LECO
-# NEON_data <- NEON_data %>%
-#   mutate(across(contains("DO"),
-#                 ~ if_else(SITE %in% c("NEON_CRAM", "NEON_LECO"), NA_real_, .)))
-# 
-# #drop temp from NEON KING 
-# 
-# NEON_data <- NEON_data %>%
-#   mutate(across(contains("temp"),
-#                 ~ if_else(SITE %in% c("NEON_KING"), NA_real_, .)))
+## LTER STEP 1: CHECK AGAINST OFFICIAL TAXONOMIC LIST FOR TYPOS/MISMATCHES ---- 
+
+#filter only LTER
+
+LTER_data <- harmonized %>% 
+  filter(grepl('LTER', SITE))
+
+unique(LTER_data$MIDSITE)
+
+#may have to do this site by site (got this from MCR metadata)
+
+MCR_taxon <- read.csv(file = file.path("data",
+                                       "MCR_LTER_Fish_Species_List.csv"))
+
+MCR_taxon <- MCR_taxon %>% 
+  select(speciesbinomial, commonname) %>% 
+  rename(COMMON_NAME = commonname,
+         SCI_NAME = speciesbinomial)
+
+#NTL site (got this from Zach)
+
+NTL_taxon <- read.csv(file = file.path("data",
+                                       "NTL_taxon.csv"))
+
+#need to change the string structure of all of these latin names. right now it's lowercase_lowercase and we need it to be "Uppercase lowercase"
+
+NTL_taxon <- NTL_taxon %>% 
+  select(!spp_code) %>% 
+  rename(COMMON_NAME = common_name,
+         SCI_NAME = latin_name) %>% 
+  mutate(SCI_NAME = str_replace_all(SCI_NAME, "_", " ") %>%
+           str_to_lower() %>%
+           str_to_title())
+
+#SBC site (got this from online metadata)
+
+SBC_taxon <- read.csv(file = file.path("data",
+                                       "SBC_LTER_species.csv")) %>% 
+  select(SCIENTIFIC_NAME,
+         COMMON_NAME) %>% 
+  rename(SCI_NAME = SCIENTIFIC_NAME)
+
+#now join all three together 
+
+LTER_taxon <- rbind(SBC_taxon,
+      MCR_taxon,
+      NTL_taxon)
+
+#now do a look at differences in taxon
+
+setdiff(LTER_data$SCI_NAME, LTER_taxon$SCI_NAME) #85 differences to take care of - woof 
+
+unique(LTER_data$SCI_NAME)
+
+#how much data at each site are unidentified species? 
+
+LTER_data %>%
+  group_by(MIDSITE) %>% 
+  summarise(
+    row_count = sum(str_detect(SCI_NAME, 
+                                regex("unidentified", 
+                                      ignore_case = TRUE))),
+    percent_unidentified = mean(str_detect(SCI_NAME, 
+                                           regex("unidentified", 
+                                                 ignore_case = TRUE))) * 100)
+#good to drop, should be around 1000 rows 
+
+LTER_data <- LTER_data %>%
+  filter(!str_detect(SCI_NAME, regex("unidentified", 
+                                     ignore_case = TRUE)) | 
+           is.na(SCI_NAME)) #leave NAs in for now 
+
+setdiff(LTER_data$SCI_NAME, LTER_taxon$SCI_NAME) #drops it down to 49 differences 
+
+## LTER STEP 2: FILTER FOR ONLY SUBSPECIES & SPECIES ---- 
+
+#pull our data that does not have a species or subspecies taxon rank
+
+LTER_data %>%
+  group_by(MIDSITE) %>% 
+  summarise(
+    row_count = sum(str_detect(SCI_NAME, 
+                               regex("spp.", 
+                                     ignore_case = TRUE))),
+    percent_unidentified = mean(str_detect(SCI_NAME, 
+                                           regex("spp.", 
+                                                 ignore_case = TRUE))) * 100)
+
+#something to potentially flag: 7% of data from VCR was "spp." -- investigate more. It appears that VCR uses spp. but in their common name has the specifics 
+
+LTER_data %>% 
+  filter(MIDSITE == "LTER_VCR") %>% 
+  filter(str_detect(SCI_NAME, "spp.")) %>% 
+  distinct(SCI_NAME, COMMON_NAME) #these are all really common and there's actually only 3. I would go in and manually replace these or drop them. 
+
+#first manually replace anchovy names, then I think we'll have to drop Pipefish since we don't know species specifically. 
+
+LTER_data$SCI_NAME[which(LTER_data$COMMON_NAME == "Bay Anchovy")] <- "Anchoa mitchilli"
+
+LTER_data$SCI_NAME[which(LTER_data$COMMON_NAME == "American Anchovy")] <- "Engraulis mordax"
+
+#now when we run this again the VCR percentage should go down. Important to note still that over 4% of all VCR data that we're dropping is Pipefish. 
+LTER_data %>%
+  group_by(MIDSITE) %>% 
+  summarise(
+    row_count = sum(str_detect(SCI_NAME, 
+                               regex("spp."))),
+    percent_unidentified = mean(str_detect(SCI_NAME, 
+                                           regex("spp."))) * 100)
+
+#double check that it is only picking up the right part of the spp. character string
+
+LTER_data %>% 
+  filter(str_detect(SCI_NAME, "spp.")) %>% 
+  distinct(SCI_NAME, COMMON_NAME) #so it's just the pipefish that are going away
+
+#should drop less than 1000 rows
+
+LTER_data <- LTER_data %>%
+  filter(!str_detect(SCI_NAME, regex("spp.", 
+                                    ignore_case = TRUE)) | 
+           is.na(SCI_NAME)) #leave NAs in for now 
+
+#now do the same thing for "sp."
+
+LTER_data %>%
+  group_by(MIDSITE) %>% 
+  summarise(
+    row_count = sum(str_detect(SCI_NAME, 
+                               regex("sp\\.", 
+                                     ignore_case = TRUE))),
+    percent_unidentified = mean(str_detect(SCI_NAME, 
+                                           regex("sp\\.", 
+                                                 ignore_case = TRUE))) * 100)
+
+#good to drop all these, all super low percentages! Should drop less than 300.
+
+LTER_data <- LTER_data %>%
+  filter(!str_detect(SCI_NAME, regex("sp\\.", 
+                                     ignore_case = TRUE)) | 
+           is.na(SCI_NAME)) #leave NAs in for now 
+
+setdiff(LTER_data$SCI_NAME, LTER_taxon$SCI_NAME) #drops it down to 44 differences 
+
+## LTER STEP 3: COMMON NAMES AT LTER NTL SITE -----
 
 
-
+#Note this is a slightly different step compared to NEON because at LTER there are no scientific names, only common. So we'll have to merge those first, then look for difference as the next taxonomic step.

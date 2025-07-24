25ded05a7aefc02218e109e0785bedf249e3d00d
bethany-williams <blw9786@gmail.com>
2025-07-24 15:19:01 -0500

    added code to check for habitat differences

--- scripts/species_exploration.R
+++ scripts/species_exploration.R
@@ -30,13 +30,17 @@ source(file = file.path("scripts",
 #species data were collated from Fish Base between June 25th and June 27th 2025
 speciesdata <- read_xlsx(path = file.path("data",
                                          "Species_attributes.xlsx"))
+#add site metadata
+sitedata <- read_xlsx(path = file.path("data",
+                                          "Site_metadata.xlsx"))
+
 
 #individual and population data are from model outputs
-inddata <- read.csv(file = file.path("data",
-                                         "species_ind_effects.csv"))
+inddata <- read_rds(file = file.path("data",
+                                         "species_ind_effects_df.RDS"))
 
-popdata <- read.csv(file = file.path("data",
-                                     "species_pop_effects.csv"))
+popdata <- read_rds(file = file.path("data",
+                                     "species_pop_effects_df.RDS"))
 
 ## combine data---------------------
 popdata <- popdata %>% 
@@ -44,30 +48,35 @@ popdata <- popdata %>%
 inddata <- inddata %>% 
   rename(SCI_NAME = s)
 
+#get overall site name for pop data and ind data
+popdata$Site<-str_sub(popdata$site,1,3)
+inddata$Site<-str_sub(inddata$site,1,3)
+sitedata$Site<-str_sub(sitedata$Site,1,3)
 
 popdata<-merge(popdata,speciesdata,by="SCI_NAME")
+popdata<-merge(popdata,sitedata,by="Site")
 inddata<-merge(inddata,speciesdata,by="SCI_NAME")
+inddata<-merge(inddata,sitedata,by="Site")
 
 
 
 
 ##Make Graphs for Individual Size DO and Temperature ---------------------
 
-#remove one massive outlier for do (-400 median)
-inddata2<-subset(inddata,SCI_NAME!="Paralichthys dentatus")
 #scatterplot function
 scatter_funind = function(x, y) {
-  ggplot(inddata2, aes(x = .data[[x]], y = .data[[y]]) ) +
+  ggplot(inddata, aes(x = .data[[x]], y = .data[[y]]) ) +
     geom_point() +
     geom_smooth(method = "lm", se = TRUE, color = "grey74") +
     theme_cowplot()+
-    ggtitle("Individual Unfiltered")
+    ggtitle("Individual Unfiltered")+
+    facet_grid(~var+Habitat)
 }
 
 #select explanatory and response variables
-explind = names(inddata2)[14:20]
+explind = names(inddata)[11:17]
 explind = set_names(explind)
-respind = names(inddata2)[c(4,8)]
+respind = names(inddata)[c(5)]
 respind = set_names(respind)
 
 all_plots_ind = purrr::map(respind, function(respind) {
@@ -85,13 +94,14 @@ scatter_funpop = function(x, y) {
     geom_point() +
     geom_smooth(method = "lm", se = TRUE, color = "grey74") +
     theme_cowplot()+
-    ggtitle("Population Unfiltered")
+    ggtitle("Population Unfiltered")+
+    facet_grid(~var+Habitat)
 }
 
 #select explanatory and response variables
-explpop = names(popdata)[14:20]
+explpop = names(popdata)[11:17]
 explpop = set_names(explpop)
-resppop = names(popdata)[c(4,8)]
+resppop = names(popdata)[c(5)]
 resppop = set_names(resppop)
 
 all_plots_pop = purrr::map(resppop, function(resppop) {
@@ -103,105 +113,65 @@ print(all_plots_pop)
 
 ## Same Graphs but with filtered data to PD>0.8 ---------------------
 
-inddatafiltereddo<-subset(inddata,PD_DO>=0.8)
-inddatafilteredtemp<-subset(inddata,PD_temp>=0.8)
-popdatafiltereddo<-subset(popdata,PD_DO>=0.8)
-popdatafilteredtemp<-subset(popdata,PD_temp>=0.8)
+inddatafiltered<-subset(inddata,PD>=0.8)
+popdatafiltered<-subset(popdata,PD>=0.8)
+
 
 
 ##Make Graphs for Individual Size DO and Temperature ---------------------
 #scatterplot function
-scatter_funindfiltereddo = function(x, y) {
-  ggplot(inddatafiltereddo, aes(x = .data[[x]], y = .data[[y]]) ) +
+scatter_funindfiltered = function(x, y) {
+  ggplot(inddatafiltered, aes(x = .data[[x]], y = .data[[y]]) ) +
     geom_point() +
     geom_smooth(method = "lm", se = TRUE, color = "lightblue") +
     theme_cowplot()+
-    ggtitle("Individual Filtered")
+    ggtitle("Individual Filtered")+
+    facet_grid(~var+Habitat)
 }
 
 #select explanatory and response variables
-explind = names(inddata)[14:20]
+explind = names(inddatafiltered)[11:17]
 explind = set_names(explind)
-respinddo = names(inddata)[4]
-respinddo = set_names(respinddo)
+respind = names(inddatafiltered)[5]
+respind = set_names(respind)
 
-all_plots_ind_filtered_do = purrr::map(respinddo, function(respinddo) {
+all_plots_ind_filtered = purrr::map(respind, function(respind) {
   purrr::map(explind, function(explind) {
-    scatter_funindfiltereddo(x = explind, y = respinddo)
+    scatter_funindfiltered(x = explind, y = respind)
   })
 })
-print(all_plots_ind_filtered_do)
+print(all_plots_ind_filtered)
 
-#scatterplot function
-scatter_funindfilteredtemp = function(x, y) {
-  ggplot(inddatafilteredtemp, aes(x = .data[[x]], y = .data[[y]]) ) +
-    geom_point() +
-    geom_smooth(method = "lm", se = TRUE, color = "lightblue") +
-    theme_cowplot()+
-    ggtitle("Individual Filtered")
-}
 
-#select explanatory and response variables
-explind = names(inddata)[14:20]
-explind = set_names(explind)
-respindtemp = names(inddata)[8]
-respindtemp = set_names(respindtemp)
-
-all_plots_ind_filtered_temp = purrr::map(respindtemp, function(respindtemp) {
-  purrr::map(explind, function(explind) {
-    scatter_funindfilteredtemp(x = explind, y = respindtemp)
-  })
-})
-print(all_plots_ind_filtered_temp)
 
 ##Make Graphs for Population Size DO and Temperature ---------------------
 #scatterplot function
-scatter_funpopfiltereddo = function(x, y) {
-  ggplot(popdatafiltereddo, aes(x = .data[[x]], y = .data[[y]]) ) +
+scatter_funpopfiltered = function(x, y) {
+  ggplot(popdatafiltered, aes(x = .data[[x]], y = .data[[y]]) ) +
     geom_point() +
     geom_smooth(method = "lm", se = TRUE, color = "lightblue") +
     theme_cowplot()+
-    ggtitle("Population Filtered")
+    ggtitle("Population Filtered")+
+    facet_grid(~var+Habitat)
 }
 
 #select explanatory and response variables
-explpop = names(popdata)[14:20]
+explpop = names(popdata)[11:17]
 explpop = set_names(explpop)
-resppopdo = names(popdata)[4]
-resppopdo = set_names(resppopdo)
+resppop = names(popdata)[5]
+resppop = set_names(resppop)
 
-all_plots_pop_filtered_do = purrr::map(resppopdo, function(resppopdo) {
+all_plots_pop_filtered = purrr::map(resppop, function(resppop) {
   purrr::map(explpop, function(explpop) {
-    scatter_funpopfiltereddo(x = explpop, y = resppopdo)
+    scatter_funpopfiltered(x = explpop, y = resppop)
   })
 })
-print(all_plots_pop_filtered_do)
+print(all_plots_pop_filtered)
 
-#scatterplot function
-scatter_funpopfilteredtemp = function(x, y) {
-  ggplot(popdatafilteredtemp, aes(x = .data[[x]], y = .data[[y]]) ) +
-    geom_point() +
-    geom_smooth(method = "lm", se = TRUE, color = "lightblue") +
-    theme_cowplot()+
-    ggtitle("Population Filtered")
-}
-
-#select explanatory and response variables
-explpop = names(popdata)[14:20]
-explpop = set_names(explpop)
-resppoptemp = names(popdata)[8]
-resppoptemp = set_names(resppoptemp)
-
-all_plots_pop_filtered_temp = purrr::map(resppoptemp, function(resppoptemp) {
-  purrr::map(explpop, function(explpop) {
-    scatter_funpopfilteredtemp(x = explpop, y = resppoptemp)
-  })
-})
-print(all_plots_pop_filtered_temp)
 
 ##Compile all graphs ---------------------
 #unfiltered
-pdf("all_scatterplots_unfiltered.pdf")
+pdf("all_scatterplots_unfiltered_habitat.pdf")
 #Length
 all_plots_ind
 #Population
@@ -210,9 +180,7 @@ dev.off()
 
 
 #filtered
-pdf("all_scatterplots_filtered.pdf")
-all_plots_ind_filtered_do
-all_plots_ind_filtered_temp
-all_plots_pop_filtered_do
-all_plots_pop_filtered_temp
+pdf("all_scatterplots_filtered_habitat.pdf")
+all_plots_ind_filtered
+all_plots_pop_filtered
 dev.off()
